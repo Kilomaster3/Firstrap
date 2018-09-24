4cf1e0816ac316b6f6b43f253956e3f625fc6320
mb <mb@MacBook-Pro-mb.local>
2018-09-24 22:01:21 +0300

    1

---
+++ zd7/zd7/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  zd7
+//
+//  Created by mb on 24.09.2018.
+//  Copyright © 2018 mb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
